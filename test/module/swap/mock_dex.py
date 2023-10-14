ac58f31a9635c2fb389bd5efb47f5919309c6ee5
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2023-10-14 17:31:28 +0300

    move some test cases from syncswap-warmup

---
+++ test/module/swap/mock_dex.py
@@ -0,0 +1,14 @@
+from sybil_engine.domain.dex import Dex
+
+
+class MockDex(Dex):
+    def __init__(self, chain_contracts, tokens, chain_instance, web3, retries, sleeping_interval, retry_counter=0):
+        self.reties = retries
+        self.retry_counter = retry_counter
+        super().__init__(chain_contracts, tokens, chain_instance, web3, sleeping_interval)
+
+    def swap(self, amount_to_swap, from_token, to_token, slippage, account):
+        if self.retry_counter < self.reties:
+            self.retry_counter += 1
+            super().swap(amount_to_swap, from_token, to_token, slippage, account)
+
---
+++ test/module/swap/test_swap_facade.py
@@ -0,0 +1,32 @@
+import unittest
+
+from loguru import logger
+from sybil_engine.data.contracts import get_contracts_for_chain
+from sybil_engine.data.networks import get_chain_instance
+from sybil_engine.data.pairs import Pairs
+from sybil_engine.data.tokens import get_tokens_for_chain
+from sybil_engine.utils.web3_utils import init_web3
+
+from core.modules.swap.swap_facade import swap_facade
+from tests.core.modules import init_set_test_config
+
+
+class TestSwapFacade(unittest.TestCase):
+    init_set_test_config()
+
+    def test_shouldGetDex(self):
+        logger.info("Test swap facade")
+
+        chain_contracts = get_contracts_for_chain("ZKSYNC")
+        tokens = get_tokens_for_chain('ZKSYNC')
+        dex_apps = swap_facade.get_swap_apps_by_chain('ZKSYNC')
+        chain_instance = get_chain_instance('ZKSYNC')
+        pairs = Pairs(swap_facade).get_pairs_by_tokens('ETH', 'USDC', 'ZKSYNC')
+        web3 = init_web3(chain_instance, None)
+
+        try:
+            for swap_app in dex_apps:
+                for pair in pairs:
+                    print(swap_facade.get_dex(chain_contracts, pair, swap_app, tokens, chain_instance, web3))
+        except Exception as e:
+            self.fail(f"Some function raised an exception: {e}")
