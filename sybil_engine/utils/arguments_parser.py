17ddb7dcff8c9b914cd60b5bee419a7948b40402
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2023-10-27 16:59:10 +0300

    add wallets configuration to arguments_parser.py

--- sybil_engine/utils/arguments_parser.py
+++ sybil_engine/utils/arguments_parser.py
@@ -1,26 +1,31 @@
 import argparse
 import os
+import sys
 
 
 def parse_arguments(default_password, default_module):
     parser = argparse.ArgumentParser(description='Process arguments.')
 
     data_folder = 'data'
+    wallets_folder = f'{data_folder}/wallets'
 
     parser.add_argument('--profile', type=str, required=False,
                         default=os.environ.get('PROFILE', 'default'),
                         help='a string to be processed')
+    parser.add_argument('--wallets', type=str, required=False,
+                        default=os.environ.get('WALLETS', wallets_folder),
+                        help='a string to be processed')
     parser.add_argument('--private_keys', type=str, required=False,
-                        default=os.environ.get('PRIVATE_KEYS', f'{data_folder}/_wallets/private_keys.txt'),
+                        default=os.environ.get('PRIVATE_KEYS', f'{wallets_folder}/private_keys.txt'),
                         help='a string to be processed')
     parser.add_argument('--cex_addresses', type=str, required=False,
-                        default=os.environ.get('CEX_ADDRESSES', f'{data_folder}/wallets/cex_addresses.txt'),
+                        default=os.environ.get('CEX_ADDRESSES', f'{wallets_folder}/cex_addresses.txt'),
                         help='a string to be processed')
     parser.add_argument('--starknet_addresses', type=str, required=False,
-                        default=os.environ.get('STARKNET_ADDRESSES', f'{data_folder}/wallets/starknet_addresses.txt'),
+                        default=os.environ.get('STARKNET_ADDRESSES', f'{wallets_folder}/starknet_addresses.txt'),
                         help='a string to be processed')
     parser.add_argument('--proxy_file', type=str, required=False,
-                        default=os.environ.get('PROXY_FILE', f'{data_folder}/wallets/proxy.txt'),
+                        default=os.environ.get('PROXY_FILE', f'{wallets_folder}/proxy.txt'),
                         help='a string to be processed')
     parser.add_argument('--password', type=str, required=False, default=os.environ.get('PASSWORD', default_password),
                         help='a string to be processed')
@@ -29,7 +34,18 @@ def parse_arguments(default_password, default_module):
     parser.add_argument('--module', type=str, required=False, default=os.environ.get('MODULE', default_module),
                         help='a string to be processed')
 
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    if '--private_keys' not in sys.argv:
+        args.private_keys = os.path.join(args.wallets, 'private_keys.txt')
+    if '--cex_addresses' not in sys.argv:
+        args.cex_addresses = os.path.join(args.wallets, 'cex_addresses.txt')
+    if '--starknet_addresses' not in sys.argv:
+        args.starknet_addresses = os.path.join(args.wallets, 'starknet_addresses.txt')
+    if '--proxy_file' not in sys.argv:
+        args.proxy_file = os.path.join(args.wallets, 'proxy.txt')
+
+    return args
 
 
 def parse_profile():
