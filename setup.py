be66cde8400509262027f5c38b373848f4e349db
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2023-10-27 16:59:14 +0300

    Bump version: 2.0.0 → 2.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r", encoding="utf-8") as file:
 
 setup(
     name='sybil_engine',
-    version='2.0.0',
+    version='2.1.0',
     py_modules=['sybil_engine'],
     packages=find_packages(),
     install_requires=requirements,
