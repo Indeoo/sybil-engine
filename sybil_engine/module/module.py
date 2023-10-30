f9ed034d3f379929f1cef3da7e85986295972d17
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2023-10-30 16:02:56 +0200

    add validate_supported_chain to Module

--- sybil_engine/module/module.py
+++ sybil_engine/module/module.py
@@ -1,5 +1,9 @@
+from sybil_engine.utils.utils import ConfigurationException
+
+
 class Module:
     module_name = 'None'
+    allowed_chains = 'None'
 
     def __init__(self, min_native_balance, account):
         self.min_native_balance = min_native_balance
@@ -18,6 +22,10 @@ class Module:
 
     def sleep_after(self):
         return True
+
+    def validate_supported_chain(self, chain):
+        if chain not in self.allowed_chains:
+            raise ConfigurationException(f"{self.module_name} does not support {chain}, skip")
 
     def set_auto_withdrawal(self, module_params):
         if module_params.get('auto_withdrawal', False):
