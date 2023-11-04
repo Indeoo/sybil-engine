d3face921b1d62e530e58942d361e6473597eaf0
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2023-11-04 12:22:05 +0200

    add @repeatable_log for RepeatableModules

--- sybil_engine/module/module_executor.py
+++ sybil_engine/module/module_executor.py
@@ -3,7 +3,6 @@ from collections import defaultdict
 
 from sybil_engine.config.app_config import get_okx
 from sybil_engine.domain.balance.balance import NotEnoughNativeBalance
-from sybil_engine.module.module import RepeatableModule
 from sybil_engine.utils.accumulator import add_accumulator_str
 from sybil_engine.utils.okx import withdrawal
 from sybil_engine.utils.utils import randomized_sleeping, ModuleException, AccountException
@@ -15,12 +14,9 @@ class ModuleExecutor:
         self.sleep_interval = sleep_interval
 
     def execute_modules(self, modules, account):
-        executed_counter = defaultdict(int)
-
         try:
             for module, module_args in modules:
-                executed_counter[type(module)] += 1
-                self.execute_module(module_args, account, module, executed_counter[type(module)])
+                self.execute_module(module_args, account, module)
         except AccountException as e:
             logger.error(f'Error, skip account {account}: {e}')
             add_accumulator_str("Failed accounts: ", account)
@@ -28,10 +24,8 @@ class ModuleExecutor:
             logger.error(f'Error, skip account {account}: {e}')
             add_accumulator_str("Failed accounts: ", account)
 
-    def execute_module(self, module_args, account, module, counter):
+    def execute_module(self, module_args, account, module):
         try:
-            if issubclass(type(module), RepeatableModule):
-                logger.info(f"({counter}/{module.repeats}) {module.module_name} {account.address}")
             parsed_module_args = module.parse_params(module_args)
             module.execute(*parsed_module_args, account)
             if module.sleep_after():
@@ -44,6 +38,6 @@ class ModuleExecutor:
             if auto_withdrawal and e.chain in ['ZKSYNC', 'LINEA'] and module.auto_withdrawal:
                 withdrawal(account.address, okx_secret, e.chain, cex_data, withdraw_interval)
                 randomized_sleeping({'from': 60 * 5, 'to': 60 * 10})
-                self.execute_module(module_args, account, module, counter)
+                self.execute_module(module_args, account, module)
             else:
                 raise AccountException(e)
