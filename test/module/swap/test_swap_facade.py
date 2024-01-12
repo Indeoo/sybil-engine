c752942ca3cc6623338676b69cac892903c548ef
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2024-01-12 16:49:51 +0200

    fix tests

--- test/module/swap/test_swap_facade.py
+++ test/module/swap/test_swap_facade.py
@@ -1,10 +1,8 @@
 import unittest
 
 from loguru import logger
-from sybil_engine.data.contracts import get_contracts_for_chain
 from sybil_engine.data.networks import get_chain_instance
 from sybil_engine.data.pairs import Pairs
-from sybil_engine.data.tokens import get_tokens_for_chain
 from sybil_engine.utils.web3_utils import init_web3
 from test import init_set_test_config
 from test.module.swap.mock_test_swap_facade import swap_facade
@@ -16,8 +14,8 @@ class TestSwapFacade(unittest.TestCase):
     def test_shouldGetDex(self):
         logger.info("Test swap facade")
 
-        dex_apps = swap_facade.get_swap_apps_by_chain('ZKSYNC')
-        chain_instance = get_chain_instance('ZKSYNC')
+        dex_apps = swap_facade.get_swap_apps_by_chain('BASE')
+        chain_instance = get_chain_instance('BASE')
         pairs = Pairs(swap_facade).get_pairs_by_tokens('ETH', 'USDC', 'ZKSYNC')
         web3 = init_web3(chain_instance, None)
 
