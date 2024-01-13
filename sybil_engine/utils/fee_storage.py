879c3a0217c57a1351062c00d3c1c7ae66d4d185
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2024-01-13 12:13:54 +0200

    fix fee_storage

--- sybil_engine/utils/fee_storage.py
+++ sybil_engine/utils/fee_storage.py
@@ -2,16 +2,9 @@ from loguru import logger
 
 from sybil_engine.domain.balance.balance_utils import from_wei_to_eth
 from sybil_engine.utils.binance_prices import get_binance_price
+from collections import defaultdict
 
-FEE = {
-    'ETH': 0,
-    'MATIC': 0,
-    'BNB': 0,
-    'AVAX': 0,
-    'FTM': 0,
-    'USDC': 0,
-    'CORE': 0
-}
+FEE = defaultdict(int)
 
 
 def add_fee(gas_token, transaction_price_wei):
@@ -22,9 +15,9 @@ def print_fee():
     converted_dict = {k: float(from_wei_to_eth(v)) for k, v in FEE.items()}
 
     amount = 0
-    for token, fee in FEE.items():
-        price = get_binance_price(token + 'USDT')
-        amount = amount + float(from_wei_to_eth(fee)) * price
+    # for token, fee in FEE.items():
+    #     price = get_binance_price(token + 'USDT')
+    #     amount = amount + float(from_wei_to_eth(fee)) * price
 
-    logger.info(f"Total fee is: {converted_dict}")
-    logger.info(f"Approximate USD cost: {amount}")
+    logger.info(f"Total native fee is: {converted_dict}")
+    #logger.info(f"Approximate USD cost: {amount}")
