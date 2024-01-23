56c0763f7d34cff0cbbaf043593e06d7595e9840
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2024-01-23 16:31:21 +0200

    add data parameter to Send

--- sybil_engine/contract/send.py
+++ sybil_engine/contract/send.py
@@ -7,12 +7,14 @@ from sybil_engine.contract.transaction_executor import evm_transaction
 class Send(Contract):
 
     @evm_transaction
-    def send_to_wallet(self, account, to_address, amount_to_swap):
+    def send_to_wallet(self, account, to_address, amount_to_swap, data=None):
         sender = Web3.to_checksum_address(account.address)
 
         txn_params = self.build_generic_data(sender, False)
         txn_params['value'] = amount_to_swap.wei
         txn_params['gasPrice'] = self.web3.eth.gas_price
         txn_params['to'] = Web3.to_checksum_address(to_address)
+        if data is not None:
+            txn_params['data'] = data
 
         return txn_params
