a2af496601b9f93c8d12e77af40eea73550859c4
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2024-02-05 17:22:20 +0200

    Add app_accounts validation fox OKX

---
+++ sybil_engine/utils/cex_utils.py
@@ -0,0 +1,9 @@
+from sybil_engine.config.app_config import get_cex_data
+from sybil_engine.utils.binance_utils import get_binance_deposit_addresses
+from sybil_engine.utils.okx_utils import get_okx_deposit_addresses
+
+
+def get_cex_addresses():
+    password, cex_data = get_cex_data()
+
+    return get_okx_deposit_addresses(password, cex_data['okx']) + get_binance_deposit_addresses(password, cex_data['binance'])
