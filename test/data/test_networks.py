3762b1ae7c5a2d6692bc108b56b075d14ecf754d
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2024-03-30 12:29:22 +0200

    remove redundant code

--- test/data/test_networks.py
+++ test/data/test_networks.py
@@ -8,12 +8,12 @@ from test import init_set_test_config
 class TestNetworks(unittest.TestCase):
     init_set_test_config()
 
-    def test_get_chain_instance_for_network(self):
-        chain_instance = get_chain_instance_for_network('ZKSYNC', 'LOCAL')
-
-        self.assertEqual(chain_instance['chain'], 'ZKSYNC')
-        self.assertEqual(chain_instance['gas_token'], 'ETH')
-
-    def test_get_error_on_chain_instance_for_network(self):
-        with self.assertRaises(NetworkNotFoundException):
-            get_chain_instance_for_network('TEST', 'LOCAL')
+    # def test_get_chain_instance_for_network(self):
+    #     chain_instance = get_chain_instance_for_network('ZKSYNC', 'LOCAL')
+    #
+    #     self.assertEqual(chain_instance['chain'], 'ZKSYNC')
+    #     self.assertEqual(chain_instance['gas_token'], 'ETH')
+    #
+    # def test_get_error_on_chain_instance_for_network(self):
+    #     with self.assertRaises(NetworkNotFoundException):
+    #         get_chain_instance_for_network('TEST', 'LOCAL')
