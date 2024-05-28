ddf3addf510577a4d3c33356f7f4b73d9cbeff96
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2024-05-28 20:33:49 +0900

    add wait for transaction exception handling

--- sybil_engine/contract/transaction_executor.py
+++ sybil_engine/contract/transaction_executor.py
@@ -1,6 +1,7 @@
 from functools import wraps
 
 from loguru import logger
+from sybil_engine.utils.retry import retry
 from web3 import Web3
 
 from sybil_engine.domain.balance.balance_utils import from_wei_to_eth
@@ -43,7 +44,7 @@ def execute_transaction_internal(func, args, chain_instance, account, web3=None)
         tx_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
 
         randomized_sleeping(chain_instance['transaction_sleep_interval'])
-        web3.eth.wait_for_transaction_receipt(tx_hash)
+        wait_for_transaction(tx_hash, web3)
 
         logger.info(f">>> {chain_instance['scan']}/tx/{Web3.to_hex(tx_hash)}")
         tx_receipt = web3.eth.get_transaction_receipt(tx_hash)
@@ -60,6 +61,11 @@ def execute_transaction_internal(func, args, chain_instance, account, web3=None)
     add_fee(chain_instance['gas_token'], transaction_price_wei)
 
     return tx_hash
+
+
+@retry(max_attempts=3, retry_interval={'from': 60 * 1, 'to': 60 * 4})
+def wait_for_transaction(tx_hash, web3):
+    web3.eth.wait_for_transaction_receipt(tx_hash)
 
 
 def evm_starknet_transaction(func):
