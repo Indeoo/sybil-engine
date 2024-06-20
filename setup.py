b16d6cb62a57c2f890e0a792c06162bc50c2b433
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2024-06-20 09:52:41 +0200

    Bump version: 7.4.2 → 7.4.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r", encoding="utf-8") as file:
 
 setup(
     name='sybil_engine',
-    version='7.4.2',
+    version='7.4.3',
     py_modules=['sybil_engine'],
     packages=find_packages(),
     install_requires=requirements,
