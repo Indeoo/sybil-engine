b7fda9338703b0a79d7320045598eeadbaab4788
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2024-07-05 17:37:01 +0300

    randomize send gas

--- sybil_engine/contract/send.py
+++ sybil_engine/contract/send.py
@@ -1,3 +1,5 @@
+import random
+
 from web3 import Web3
 
 from sybil_engine.contract.contract import Contract
@@ -12,7 +14,7 @@ class Send(Contract):
 
         txn_params = self.build_generic_data(sender, False)
         txn_params['value'] = amount_to_swap.wei
-        txn_params['gasPrice'] = self.web3.eth.gas_price * 3
+        txn_params['gasPrice'] = self.web3.eth.gas_price * random.uniform(1.1, 1.2)
         txn_params['to'] = Web3.to_checksum_address(to_address)
         if data is not None:
             txn_params['data'] = data
