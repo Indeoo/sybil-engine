78a780d4c42dd446f92bd8702c4c66f9920e9acd
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2025-01-23 13:21:04 +0200

    Bump version: 9.2.3 → 9.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r", encoding="utf-8") as file:
 
 setup(
     name='sybil_engine',
-    version='9.2.3',
+    version='9.3.0',
     py_modules=['sybil_engine'],
     packages=find_packages(),
     install_requires=requirements,
