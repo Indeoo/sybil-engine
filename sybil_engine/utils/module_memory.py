51dfeeefb28b0e790b3f8033826ec23e9dac0554
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2025-01-28 15:35:13 +0200

    fix accumulate_by_key

--- sybil_engine/utils/module_memory.py
+++ sybil_engine/utils/module_memory.py
@@ -10,7 +10,10 @@ def add_memory_list(key, value):
 def accumulate_by_key(key, value):
     if key not in memory:
         memory[key] = []
-    memory[key] += value
+    if isinstance(value, map):
+        memory[key].append(list(value))
+    else:
+        memory[key] += value
 
 
 def remove_key(key):
