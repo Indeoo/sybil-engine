1ea8433e5a646f8c3935dd7af6fb1cde3f71da0a
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2025-01-29 00:38:02 +0200

    Bump version: 10.0.7 → 10.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r", encoding="utf-8") as file:
 
 setup(
     name='sybil_engine',
-    version='10.0.7',
+    version='10.0.8',
     py_modules=['sybil_engine'],
     packages=find_packages(),
     package_data={
