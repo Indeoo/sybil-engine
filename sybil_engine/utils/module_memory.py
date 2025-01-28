fbb15d486360c2e64ce077aef6361531d2f23e59
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2025-01-28 15:12:45 +0200

    add module_memory.py

---
+++ sybil_engine/utils/module_memory.py
@@ -0,0 +1,22 @@
+memory = {
+
+}
+
+
+def add_memory_list(key, value):
+    memory[key] = value
+
+
+def accumulate_by_key(key, value):
+    if key in memory:
+        memory[key] += value
+    else:
+        memory[key] = [value]
+
+
+def remove_key(key):
+    del memory[key]
+
+
+def get_by_key(key):
+    return memory[key]
